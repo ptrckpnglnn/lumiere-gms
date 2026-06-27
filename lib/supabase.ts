f864eeeedaa460a5b1f21d5ec8d51a03ce02c571
ptrckpnglnn <ptrckpnglnn@gmail.com>
2026-06-27 20:52:22 +0800

    initial commit

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pkevfbklrgrxllniofhv.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrZXZmYmtscmdyeGxsbmlvZmh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxMDU2NTMsImV4cCI6MjA5NzY4MTY1M30.a8mBDuETQSN7yMg_HRyNc87Rzj7F9KFluyUkK3S7mqg";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
